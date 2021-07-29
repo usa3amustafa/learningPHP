d79ace7675a90b98303857435fc1401aadec2151
usa3amustafa <usa3a3ustafa@outlook.com>
2021-07-29 21:28:36 +0500

    29_07

--- 11_if_else_statement.php
+++ 11_if_else_statement.php
@@ -7,6 +7,30 @@
   <body>
     <?php
 
+      $a = 10;
+
+      $name =  "Usama Mustafa";
+      $age = 25;
+
+      if ($a < 5) {
+        // code...
+        echo "x is smaller than 5";
+        echo "<br>";
+
+      }else {
+        // code...
+        echo "x is not smaller than 5";
+        echo "<br>";
+      }
+
+      if ($age > 18) {
+        // code...
+        echo "Hello Mr, {$name} you are Eligible" ;
+      }else {
+        // code...
+        echo "Hello Mr. {$name} you are not Eligible";
+      }
+
     ?>
   </body>
 </html>
