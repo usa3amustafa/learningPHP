bac7e633568f12890b2967b70da7443d8fd4e647
usa3amustafa <usa3a3ustafa@outlook.com>
2021-08-01 13:41:16 +0500

    01_08

---
+++ 32_multidimentional_arrays.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Multidimentional Arrays</title>
+  </head>
+  <body>
+    <?php
+
+    #mutidimentional indexed array
+      $array = [
+        [1 , "John" ,  "Manager" , 50000] ,
+        [2 , "Mark" ,  "Salesman" , 20000] ,
+        [3 , "Kelly" ,  "Computer Operator" , 12000 , "married"] ,
+        [4 , "Stella" ,  "Driver" , 5000] ,
+      ];
+
+      echo "<pre>";
+      print_r($array) ;
+      echo "</pre>";
+
+      // echo "<pre>";
+      // var_dump($array) ;
+      // echo "</pre>";
+
+      $arrLength = count($array);
+
+      for ($i=0; $i < $arrLength ; $i++) {
+        // code...
+        $arrLength2 = count($array[$i]);
+        for ($j=0; $j < $arrLength2 ; $j++) {
+          // code...
+          echo $array[$i][$j]." ";
+        }
+        echo "<br>";
+      }
+
+      echo "<br>";
+
+      echo "<table border = '5px'>";
+      foreach ($array as $row) {
+        echo "<tr>";
+        foreach ($row as $col) {
+            echo "<td>" .$col. "</td> ";
+          }
+          echo "</tr>";
+      }
+      echo "</table>";
+
+    ?>
+  </body>
+</html>
